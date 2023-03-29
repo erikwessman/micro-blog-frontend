8745e7043b3b35f917f98420491296dc7e6bb0ec
Erik Wessman <erikwessman10@gmail.com>
2023-03-29 19:05:05 +0200

    Fixed port env

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
 const backend = process.env.REACT_APP_BACKEND_HOST || 'localhost';
-const port = process.env.REACT_APP_BACKEND_PORT || '5000';
+const port = process.env.REACT_APP_BACKEND_PORT;
 const protocol = process.env.REACT_APP_BACKEND_PROTOCOL || 'http';
 
-const backendApiEndpoint = protocol + '://' + backend + ':' + port + '/api';
+let backendApiEndpoint: string = ''
+
+if (port === undefined) {
+  backendApiEndpoint = protocol + '://' + backend + '/api';
+} else {
+  backendApiEndpoint = protocol + '://' + backend + ':' + 5000 + '/api';
+}
+
 console.log('Using backend endpoint: ' + backendApiEndpoint);
 
 export const api = axios.create({
-    baseURL: backendApiEndpoint
-  });
-  
+  baseURL: backendApiEndpoint
+});
